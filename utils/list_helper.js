1ce2c105f5ae54f09c4d70d84c42257cead1abd8
EileenJuergens <juergens.eileen@gmail.com>
2020-05-23 18:09:21 +0200

    feat: finish part 4.4 - helper functions and unit tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,7 +4,24 @@ const dummy = (blogs) => (
     : undefined
 );
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0;
+  }
+
+  if (blogs.length === 1) {
+    return blogs[0].likes;
+  }
+
+  if (blogs.length > 1) {
+    const initialValue = 0;
+    return blogs.reduce((acc, curr) => {
+      return acc + curr.likes;
+    }, initialValue);
+  }
+};
 
 module.exports = {
   dummy,
+  totalLikes,
 };
