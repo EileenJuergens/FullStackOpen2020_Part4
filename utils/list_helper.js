fe9982f8ddc9616b19045f450aa1e54e6c315c4a
EileenJuergens <juergens.eileen@gmail.com>
2020-05-24 19:01:05 +0200

    feat: finish part 4.6 - helper functions and unit tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,6 +4,7 @@ const dummy = (blogs) => (
     : undefined
 );
 
+
 const totalLikes = (blogs) => {
   if (blogs.length === 0) {
     return 0;
@@ -21,14 +22,35 @@ const totalLikes = (blogs) => {
   }
 };
 
+
 const favoriteBlog = (blogs) => {
   const maxValue = Math.max(...blogs.map((blog) => blog.likes), 0);
   const favoriteBlogs = blogs.filter((blog) => blog.likes === maxValue);
   return favoriteBlogs[0];
 };
 
+
+const mostBlogs = (blogs) => {
+  const authors = [];
+  blogs.map((blog) => authors.push(blog.author));
+  const allAuthors = Array.from(new Set(authors));
+
+  const authorsBlogs = [];
+  allAuthors.map((author, i) => {
+    const counter = blogs.filter((blog) => blog.author === allAuthors[i]).length;
+    authorsBlogs.push({ author, blogs: counter });
+  });
+
+  const maxValue = Math.max(...authorsBlogs.map((author) => author.blogs));
+  const authorWithMostBlogs = authorsBlogs.filter((obj) => obj.blogs === maxValue);
+
+  return authorWithMostBlogs[0];
+};
+
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
