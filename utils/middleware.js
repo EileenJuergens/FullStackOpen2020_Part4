9252bd3475dc6ee32521cfcea128b0571c382c67
EileenJuergens <juergens.eileen@gmail.com>
2020-06-27 19:17:23 +0200

    feat: finish part 4.19 - Bloglist expansions

--- utils/middleware.js
+++ utils/middleware.js
@@ -18,11 +18,15 @@ const errorHandler = (error, request, response, next) => {
     response
       .status(400)
       .send({ error: 'malformatted id' });
-  } else if (error.name === 'ValidationError') {
+  }
+
+  if (error.name === 'ValidationError') {
     response
       .status(400)
       .json({ error: error.message });
-  } else if (error.name === 'JsonWebTokenError') {
+  }
+
+  if (error.name === 'JsonWebTokenError') {
     response
       .status(401)
       .json({ error: 'invalid token' });
